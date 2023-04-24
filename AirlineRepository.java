0671700fb29962ba14dbdc2edbb3f4d1b6661354
SASI AL <120035808+sasi1616@users.noreply.github.com>
2023-04-24 06:32:50 -0700

    Delete AirlineRepository.java

--- AirlineRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Airline.Reservation.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.Airline.Reservation.Model.AirlineReservation;
-//AirlineRepository interface extends jpa repository to use hibernate methods
-public interface AirlineRepository extends JpaRepository<AirlineReservation,Integer> {
-
-}
